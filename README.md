71099b902c1107329171c5f321d82b70ebd5c8ee
joni moas <setpsistariasbaniou@gmail.com>
2025-02-12 20:14:13 +0100

    full automation except certbot

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-Turnkey wordpress is a small shell script that can turn a [turnkey linux](https://www.turnkeylinux.org/) machine into a wordpress server,
+Turnkey wordpress is a small shell script that can turn a [turnkey linux](https://www.turnkeylinux.org/) and prolly every debian linux machine into a wordpress server,
 including a free domain from noip.com and a certificate from let's encrypt!
 
 First you have to register on [noip.com](https://www.noip.com/), make a domain and keep the ddns username and password.
 
 Enter that info in the script, as well as the name of the domain and the database root password.
 
-You will be requested to enter the root password some more times during the execution of the script, as well as some additional info for the certificates.
+You will be requested to enter some additional info for the certificates.
 
 Be sure to have activated port forwarding on your router before running the scipt!
 
--- wordpress_setup.sh
+++ wordpress_setup.sh
@@ -8,7 +8,7 @@ WORDPRESS_ROOT_PASS=
 
 #install php, nysql and libs
 apt install apache2 mariadb-server ghostscript libapache2-mod-php php curl php-gd php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-mysql php-xml php-zip snapd -y
-read -p "Libs installed"
+echo "Libs installed"
 
 #download and extract wordpress
 cd /var/www/html/
@@ -18,20 +18,21 @@ tar xzf latest.tar.gz
 mv wordpress/* .
 rm -rf wordpress latest.tar.gz
 chown -R www-data: .
-read -p "Wordpress extracted"
+echo "Wordpress extracted"
 
 #install noip
 cd /root
-wget --content-disposition https://www.noip.com/download/linux/latest
-tar xf noip-duc_3.3.0.tar.gz
-cd noip-duc_3.3.0/binaries
-chmod 777 noip-duc_3.3.0_amd64.deb
-apt install ./noip-duc_3.3.0_amd64.deb
-cd /root/noip-duc_3.3.0/debian
+rm -rf no-ip
+mkdir no-ip
+wget --content-disposition -O noip-duc.tar.gz https://www.noip.com/download/linux/latest
+tar -xf noip-duc.tar.gz -C ./no-ip --strip-components=1
+chmod 777 /root/no-ip/binaries/*.deb
+apt install /root/no-ip/binaries/*amd64.deb
+cd "/root/no-ip/debian"
 cp service /etc/systemd/system/noip-duc.service
-read -p "noip installed"
+echo "noip installed"
 
-read -p "starting noip and setting up systemd service"
+echo "starting noip and setting up systemd service"
 systemctl daemon-reload
 systemctl enable noip-duc.service
 touch /etc/default/noip-duc
@@ -41,15 +42,21 @@ echo NOIP_PASSWORD=$DDNS_PASS >> noip-duc
 echo NOIP_HOSTNAMES=$HOSTNAME >> noip-duc
 systemctl start noip-duc.service
 
-read -p "starting db install, enter root pass and answer Y to all"
+echo "starting db install"
 #database installation
-mysql_secure_installation
-read -p "please add two more times the db root pass"
-mysql -uroot -p -e 'create database wordpress;'
-mysql -uroot -p -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY '$WORDPRESS_ROOT_PASS';"
+mysql_secure_installation <<EOF
+$WORDPRESS_ROOT_PASS
+Y
+Y
+Y
+Y
+Y
+EOF
+mysql -uroot -p"$WORDPRESS_ROOT_PASS" -e 'create database wordpress;'
+mysql -uroot -p"$WORDPRESS_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY '$WORDPRESS_ROOT_PASS';"
 
 #certificates installation
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 certbot --apache
-read -p "installed certificates, setup complete"
+echo "setup complete"
